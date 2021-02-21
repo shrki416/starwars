852f9b2426422681e6d8be3b4e0c020d9e41ad8f
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-21 18:52:19 -0500

    Get Character Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const url = `https://swapi.dev/api/people`;
+
+  const getCharacters = async () => {
+    for (const character of characters) {
+      const homeworldResponse = await character.homeworld;
+      const homeworld = await axios
+        .get(homeworldResponse)
+        .then((res) => res.data);
+      character.homeworld = homeworld.name;
+
+      setCharacters(character);
+      // console.log(character.species);
+    }
+
+    axios
+      .get(url)
+      .then(({ data }) => setCharacters(data.results))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  console.log(characters);
+
   return (
     <div className="App">
       <h1>Starwars</h1>

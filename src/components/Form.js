027b26dc97ab75b03e357be0dc46613b5c77f8cc
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-02-27 20:22:25 -0500

    Refactor App component and add Form componenet

---
+++ src/components/Form.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Input, Segment } from "semantic-ui-react";
+
+function Form({ search, handleChange }) {
+  return (
+    <form onSubmit={search}>
+      <Segment basic textAlign="center">
+        <Input
+          action={{ color: "yellow", content: "Search" }}
+          iconPosition="left"
+          placeholder="Character Search"
+          type="text"
+          //   value={search}
+          onChange={handleChange}
+        />
+      </Segment>
+    </form>
+  );
+}
+
+export default Form;

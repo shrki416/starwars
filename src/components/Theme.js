946517a46c33ea2cfa8096b1b7ab906bc64ce7b6
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-04 18:10:13 -0500

    Create dark and light themes and create a toggle component

---
+++ src/components/Theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  body: "#E2E2E2",
+  text: "#363537",
+  toggleBorder: "#009432",
+  gradient: "linear-gradient(#39598A, #79D7ED)",
+};
+
+export const darkTheme = {
+  body: "#363537",
+  text: "#FFE81F",
+  toggleBorder: "#e74c3c",
+  gradient: "linear-gradient(#091236, #1E215D)",
+};
---
+++ src/components/Toggle.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { func, string } from "prop-types";
+import styled from "styled-components";
+
+import { ReactComponent as Rebels } from "../images/rebels.svg";
+import { ReactComponent as Empire } from "../images/empire.svg";
+
+const ToggleContainer = styled.button`
+  background: ${({ theme }) => theme.gradient};
+  border: 2px solid ${({ theme }) => theme.toggleBorder};
+  border-radius: 30px;
+  cursor: pointer;
+  display: flex;
+  font-size: 0.25rem;
+  justify-content: space-between;
+  margin: 0 auto;
+  overflow: hidden;
+  padding: 0.5rem;
+  position: relative;
+  width: 8rem;
+  height: 4rem;
+
+  svg {
+    height: auto;
+    width: 2.5rem;
+    transition: all 0.3s linear;
+
+    // sun icon
+    &:first-child {
+      transform: ${({ lightTheme }) =>
+        lightTheme ? "translateY(0)" : "translateY(100px)"};
+    }
+
+    // moon icon
+    &:nth-child(2) {
+      transform: ${({ lightTheme }) =>
+        lightTheme ? "translateY(-100px)" : "translateY(0)"};
+    }
+  }
+`;
+
+const Toggle = ({ theme, toggleTheme }) => {
+  const isLight = theme === "light";
+  return (
+    <ToggleContainer lightTheme={isLight} onClick={toggleTheme}>
+      <Rebels />
+      <Empire />
+    </ToggleContainer>
+  );
+};
+
+Toggle.propTypes = {
+  theme: string.isRequired,
+  toggleTheme: func.isRequired,
+};
+
+export default Toggle;

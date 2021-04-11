a12aa0cab66929da50e16f4880717725f9d690ab
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-04-11 15:15:22 -0400

    clean up comments

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -26,13 +26,11 @@ const ToggleContainer = styled.button`
     width: 2.5rem;
     transition: all 0.3s linear;
 
-    // sun icon
     &:first-child {
       transform: ${({ lightTheme }) =>
         lightTheme ? "translateY(0)" : "translateY(100px)"};
     }
 
-    // moon icon
     &:nth-child(2) {
       transform: ${({ lightTheme }) =>
         lightTheme ? "translateY(-100px)" : "translateY(0)"};

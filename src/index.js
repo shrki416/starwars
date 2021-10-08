1f9902d0c4c4f7330bf0fa62ee720b94f9adb8c8
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-07 20:20:20 -0400

    Minor CSS Changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
 import "./fonts/Starjedi.ttf";
 import "./fonts/Starjout.ttf";
@@ -13,8 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

2b7fe6db2235fe23f0a8ef48d8d1dfc3e37b4e29
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-14 20:21:13 -0400

    Putting out 🔥s

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "semantic-ui-css/semantic.min.css";
-import "./fonts/Starjedi.ttf";
-import "./fonts/Starjout.ttf";
+import "./assets/fonts/Starjedi.ttf";
+import "./assets/fonts/Starjout.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
